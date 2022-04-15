b4a42fb35e9d1505d5af16dd853d5b240248c4e2
Maverick <Mav33987@gmail.com>
2022-04-14 20:22:46 -0500

    set up imports in app.js to use components in the function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Browser as Router, Routers, Route, Navigate } from "react-router-dom";
+import Nav from './components/Nav';
+import About from './components/About';
+import Home from './components/Home';
+import Recipes from './components/Recipes';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import Gallery from './components/Gallery';
 
 function App() {
   return (
