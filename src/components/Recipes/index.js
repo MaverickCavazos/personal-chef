71bbe348c30d844c38c0b4377914fd685696c519
Oliver C <ocronk10@gmail.com>
2022-04-19 17:08:19 -0500

    Added base html for recipes page, uncommented route path for recipes as well as the import

---
+++ src/components/Recipes/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Recipes() {
+    
+  return (
+    <section>
+      <div class="row">
+        <div class="column"> Recipe #1 </div>
+        <div class="column"> Recipe #2 </div>
+        <div class="column"> Recipe #3 </div>
+    </div> 
+    </section>
+  );
+}
+
+export default Recipes
