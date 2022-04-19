2b6a778975de6a8bd04ec37bd11f7a33b0b69def
Oliver C <ocronk10@gmail.com>
2022-04-19 17:28:00 -0500

    Base html structure for recipes including 3 columns, choice of image below, and an unordered list of the ingredients

--- src/components/Recipes/index.js
+++ src/components/Recipes/index.js
@@ -1,14 +1,72 @@
 import React from "react";
+import Recipe1 from "../images/perfectparfait.jpg";
 
 function Recipes() {
-    
+
   return (
     <section>
       <div class="row">
-        <div class="column"> Recipe #1 </div>
-        <div class="column"> Recipe #2 </div>
-        <div class="column"> Recipe #3 </div>
-    </div> 
+        <div class="column">
+          <h1>Perfect Parfait</h1>
+          <div>
+            <h1 id="recipe"></h1>
+            <img
+              src={Recipe1}
+              id="recipeimg"
+              className="my-2"
+              style={{ height: "auto", width: "33%" }}
+            />
+          </div>
+          <ul>
+            <li>3/4 cups of cooked tri-color quinoa</li>
+            <li>4 oz of vanilla greek yogurt</li>
+            <li>1/2 pint of fresh mixed berries</li>
+            <li>2 tbsp of confection sugar</li>
+            <li>1 tbsp of honey</li>
+            <li>1 tbsp of raw oats</li>
+          </ul>
+        </div>
+        <div class="column">
+          <h1>Recipe #2</h1>
+          <div>
+            <h1 id="recipe"></h1>
+            <img
+              src={Recipe1}
+              id="myprofileimage"
+              className="my-2"
+              style={{ height: "auto", width: "33%" }}
+            />
+          </div>
+          <ul>
+            <li>3/4 cups of cooked tri-color quinoa</li>
+            <li>4 oz of vanilla greek yogurt</li>
+            <li>1/2 pint of fresh mixed berries</li>
+            <li>2 tbsp of confection sugar</li>
+            <li>1 tbsp of honey</li>
+            <li>1 tbsp of raw oats</li>
+          </ul>
+        </div>
+        <div class="column">
+          <h1>Recipe #3</h1>
+          <div>
+            <h1 id="recipe"></h1>
+            <img
+              src={Recipe1}
+              id="recipeimg"
+              className="my-2"
+              style={{ height: "auto", width: "33%" }}
+            />
+          </div>
+          <ul>
+            <li>3/4 cups of cooked tri-color quinoa</li>
+            <li>4 oz of vanilla greek yogurt</li>
+            <li>1/2 pint of fresh mixed berries</li>
+            <li>2 tbsp of confection sugar</li>
+            <li>1 tbsp of honey</li>
+            <li>1 tbsp of raw oats</li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 }
