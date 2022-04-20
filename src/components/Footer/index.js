34197736decf1672fe993f28fa13abac89b250e6
Oliver C <ocronk10@gmail.com>
2022-04-19 20:27:08 -0500

    Created footer & styled it

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <section>
+      <div class="footer col-lg-6 pos-middle text-left">© 2022 Chef Foluke. All rights reserved</div>
+    </section>
+  );
+}
+
+export default Footer;
