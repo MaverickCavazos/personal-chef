44fa2dff86a536c69265ee3165bb4c2b4a7ba732
Oliver C <ocronk10@gmail.com>
2022-04-21 13:55:38 -0500

    Fully functional and styled slide cards displaying several dishes

--- client/src/components/Recipes/index.js
+++ client/src/components/Recipes/index.js
@@ -21,82 +21,119 @@ export default class extends React.Component {
         naturalSlideHeight={125}
         totalSlides={5}
       >
-        <Slider>
-          <Slide index={0}>
-            <div class="card">
-              <h1>Morning fuel!</h1>
-              <br></br>
-              <img
-                class="cardimg"
-                src={Recipe1}
-                id="recipeimg"
-                className="my-2"
-                style={{ height: "25%", width: "25%", borderRadius: "5px" }}
-              />
-              <ul>
-                <h3>Overnight oats soaked in coconut milk, cinnamon and raw honey topped with fresh berries and granola. Let's not skip the most important meal of the day!</h3>
-              </ul>
-            </div>
-          </Slide>
-          <Slide index={1}>
-            <div class="card">
-              <h1>NY Strip</h1>
-              <br></br>
-              <img
-                class="cardimg"
-                src={Recipe2}
-                id="recipeimg"
-                className="my-2"
-                style={{ height: "25%", width: "25%", borderRadius: "5px" }}
-              />
-              <ul>
-                <h3>NY Strip Steak topped with blistered tomatoes and a side of roasted marble and sweet potato hash with a sunny side up egg and everything aoli drizzle!</h3>
-              </ul>
-            </div>
-          </Slide>
-          <Slide index={2}>
-            <div class="card">
-              <h1>Snow Crab Legs</h1>
-              <br></br>
-              <img
-                class="cardimg"
-                src={Recipe3}
-                id="recipeimg"
-                className="my-2"
-                style={{ height: "25%", width: "25%", borderRadius: "5px" }}
-              />
-              <h3>Roasted garlic marble potatoes with grilled red peppers, zucchini and squash! Added citrus garlic butter. Great dinner for any day of the week.</h3>        
-            </div>
-          </Slide>
-          <Slide index={3}>
-            <div class="card">
-              <h1>7-Veggie Mixed Salad</h1>
-              <br></br>
-              <img
-                class="cardimg"
-                src={Recipe4}
-                id="recipeimg"
-                className="my-2"
-                style={{ height: "25%", width: "25%", borderRadius: "5px" }}
-              />
-              <h3>7 veggie mixed salad with grilled salmon marinated in hot honey, fresh garlic, soy sauce, scallions and S&P! Topped with fresh tomatoes and scallions. If you're cutting carbs, this dish is for you. Very flavorful dish!</h3>
-            </div>
-          </Slide>
-          <Slide index={4}>
-            <div class="card">
-              <h1>Whole Snapper</h1>
-              <br></br>
-              <img
-                class="cardimg"
-                src={Recipe5}
-                id="recipeimg"
-                className="my-2"
-                style={{ height: "25%", width: "25%", borderRadius: "5px" }}
-              />
-              <h3>Dinner is served: whole snapper with sweet onions, shallots, bell pepper trio, whole roasted garlic cloves, and thyme, the fresh stuff not dried and some spices! Stewed spinach wtih caramelized sweet onion, nutmeg and coconut milk! A side of white rice with coconut milk as well. Dish is also garnished with cilantro.</h3>
-            </div>
-          </Slide>
-        </Slider>
+        <div id="main-container">
+          <Slider>
+            <Slide index={0}>
+              <div id="content">
+                <br></br>
+                <img
+                  class="homeLeft"
+                  src={Recipe1}
+                  id="recipeimg"
+                  className="my-2"
+                  style={{ height: "33%", width: "33%", borderRadius: "5px" }}
+                />
+                <div class="homeRight">
+                  <h1>Morning Fuel</h1>
+                  <br></br>
+                  <h3>
+                    Overnight oats soaked in coconut milk, cinnamon and raw
+                    honey topped with fresh berries and granola. Let's not skip
+                    the most important meal of the day!
+                  </h3>
+                </div>
+              </div>
+            </Slide>
+            <Slide index={1}>
+              <div id="content">
+                <br></br>
+                <img
+                  class="homeLeft"
+                  src={Recipe2}
+                  id="recipeimg"
+                  className="my-2"
+                  style={{ height: "25%", width: "25%", borderRadius: "5px" }}
+                />
+                <div class="homeRight">
+                <h1>NY Strip</h1>
+                <br></br>
+                <h3>
+                  NY Strip Steak topped with blistered tomatoes and a side of
+                  roasted marble and sweet potato hash with a sunny side up egg
+                  and everything aoli drizzle!
+                </h3>
+                </div>
+              </div>
+            </Slide>
+            <Slide index={2}>
+              <div id="content">
+                <br></br>
+                <img
+                  class="homeLeft"
+                  src={Recipe3}
+                  id="recipeimg"
+                  className="my-2"
+                  style={{ height: "25%", width: "25%", borderRadius: "5px" }}
+                />
+                <div class="homeRight">
+                  <h1>Snow Crab Legs</h1>
+                  <br></br>
+                  <h3>
+                    Roasted garlic marble potatoes with grilled red peppers,
+                    zucchini and squash! Added citrus garlic butter. Great
+                    dinner for any day of the week.
+                  </h3>
+                </div>
+              </div>
+            </Slide>
+            <Slide index={3}>
+              <div id="content">
+                <br></br>
+                <img
+                  class="homeLeft"
+                  src={Recipe4}
+                  id="recipeimg"
+                  className="my-2"
+                  style={{ height: "25%", width: "25%", borderRadius: "5px" }}
+                />
+                <div class="homeRight">
+                  <h1>7-Veggie Mixed Salad</h1>
+                  <br></br>
+                  <h3>
+                    7 veggie mixed salad with grilled salmon marinated in hot
+                    honey, fresh garlic, soy sauce, scallions and S&P! Topped
+                    with fresh tomatoes and scallions. If you're cutting carbs,
+                    this dish is for you. Very flavorful dish!
+                  </h3>
+                </div>
+              </div>
+            </Slide>
+            <Slide index={4}>
+              <div id="content">
+                <br></br>
+                <img
+                  class="homeLeft"
+                  src={Recipe5}
+                  id="recipeimg"
+                  className="my-2"
+                  style={{ height: "25%", width: "25%", borderRadius: "5px" }}
+                />
+                <div class="homeRight">
+                  <h1>Whole Snapper</h1>
+                  <br></br>
+                  <h3>
+                    Dinner is served: whole snapper with sweet onions, shallots,
+                    bell pepper trio, whole roasted garlic cloves, and thyme,
+                    the fresh stuff not dried and some spices! Stewed spinach
+                    wtih caramelized sweet onion, nutmeg and coconut milk! A
+                    side of white rice with coconut milk as well. Dish is also
+                    garnished with cilantro.
+                  </h3>
+                </div>
+              </div>
+            </Slide>
+          </Slider>
+        </div>
         <ButtonBack>Back</ButtonBack>
         <ButtonNext>Next</ButtonNext>
       </CarouselProvider>
